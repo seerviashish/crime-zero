b1b4ec1f9783fa4dff4f8aedfc9d9cf2ba2527bc
Ashish Seervi <seerviashish17@gmail.com>
2020-07-22 18:55:03 +0530

    ref: finished recuired component

--- client/src/components/SmsScreen/index.tsx
+++ client/src/components/SmsScreen/index.tsx
@@ -1,6 +1,9 @@
 import React, { CSSProperties } from "react";
+import Done from "../../assets/done-24px.svg";
+import DoneAll from "../../assets/done_all-24px.svg";
+import SmsError from "../../assets/error-24px.svg";
+
 import "./style.css";
-import { url } from "inspector";
 
 type Props = {
   style?: CSSProperties;
@@ -9,6 +12,16 @@ type Props = {
 const SmsScreen: React.FC<Props> = ({ style }) => {
   return (
     <div className="SmsScreen" style={{ ...style }}>
+      <div className="SmsListView">
+        <ul>
+          <li>
+            <span className="SmsText">sda</span>
+            <img className="SmsIcon" src={Done} />
+            <img className="SmsIcon" src={DoneAll} />
+            <img className="SmsIcon" src={SmsError} />
+          </li>
+        </ul>
+      </div>
       <div className="SmsInputField">
         <input className="SmsInputBox" type="text" />
         <button className="SendBtn"></button>
