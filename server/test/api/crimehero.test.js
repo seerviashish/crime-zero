af90f0ddf8a12b9e44f70e67284d1943b5351c7f
Ashish Seervi <seerviashish17@gmail.com>
2020-07-25 01:27:59 +0530

    fix: 🐛 regex g

--- server/test/api/crimehero.test.js
+++ server/test/api/crimehero.test.js
@@ -31,32 +31,35 @@ describe('API TEST', () => {
   });
 
   describe('GET /hero?code=0<space><code>', () => {
-    it('Get /hero?code=0 8467 api call with x-client-id header should give json and response code 200.', done => {
-      request(server)
+    it('Get /hero?code=0 8467 api call with x-client-id header should give json and response code 200.', () => {
+      return request(server)
         .get('/hero?code=0 8467')
         .set('x-client-id', 'crimehero123')
         .expect('Content-Type', /json/)
         .expect(httpStatus.OK)
-        .expect(200, done);
+        .expect(200);
     });
-    it('Get /hero?code=0 8467 api call without x-client-id header should give response code 404.', done => {
-      request(server).get('/hero?code=0 8467').expect(404, done);
+    it('Get /hero?code=0 8467 api call without x-client-id header should give response code 403.', () => {
+      return request(server)
+        .get('/hero?code=0 8467')
+        .expect(httpStatus.FORBIDDEN)
+        .expect(403)
+        .expect('Content-Type', /json/);
     });
-    it('Get /hero api call without x-client-id header should give response code 403.', done => {
-      request(server)
+    it('Get /hero api call without x-client-id header should give response code 403.', () => {
+      return request(server)
         .get('/hero')
         .expect('Content-Type', /json/)
         .expect(httpStatus.FORBIDDEN)
-        .expect(403, done);
+        .expect(403);
     });
-    it('Get /hero api call with x-client-id header should give text/html and response code 404.', () => {
+    it('Get /hero api call with x-client-id header should give json and response code 403.', () => {
       return request(server)
         .get('/hero')
         .set('x-client-id', 'crimehero123')
-        .expect('Content-Type', /text/)
-        .expect('Content-Type', /html/)
-        .expect(httpStatus.NOT_FOUND)
-        .expect(404);
+        .expect('Content-Type', /json/)
+        .expect(httpStatus.FORBIDDEN)
+        .expect(403);
     });
     it('Get /hero?code=0 8467 api call with validated code and x-client-id header should give json and response code 200.', async () => {
       return request(server)
@@ -69,14 +72,13 @@ describe('API TEST', () => {
           hero: 'THOR'
         });
     });
-    it('Get /hero?code=0 80267 api call with not validated code and x-client-id header should give html/text and response code 404.', async () => {
+    it('Get /hero?code=0 80267 api call with not validated code and x-client-id header should give json and response code 403.', async () => {
       return request(server)
         .get('/hero?code=0 80267')
         .set('x-client-id', 'crimehero123')
-        .expect('Content-Type', /html/)
-        .expect('Content-Type', /text/)
-        .expect(httpStatus.NOT_FOUND)
-        .expect(404);
+        .expect('Content-Type', /json/)
+        .expect(httpStatus.FORBIDDEN)
+        .expect(403);
     });
     it('Get /hero?code=0 5555 api call with validated code and x-client-id header should give json and response code 403.', async () => {
       return request(server)
